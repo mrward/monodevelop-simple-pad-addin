ef78c77b610570161ede30b0d8f1edcb6ddba039
Matt Ward <ward.matt@gmail.com>
2014-06-15 15:48:15 +0100

    Create a simple addin that adds a new Pad to MonoDevelop.

---
+++ MonoDevelopSimplePadAddin/SimplePad.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using MonoDevelop.Ide.Gui;
+
+namespace MonoDevelopSimplePadAddin
+{
+	public class SimplePad : IPadContent
+	{
+		Gtk.Label label;
+
+		public void Initialize (IPadWindow window)
+		{
+			label = new Gtk.Label ("Simple pad label");
+			label.Visible = true;
+		}
+
+		public void RedrawContent ()
+		{
+		}
+
+		public Gtk.Widget Control {
+			get { return label; }
+		}
+
+		public void Dispose ()
+		{
+		}
+	}
+}
+
---
+++ MonoDevelopSimplePadAddin/package.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files (x86)\Xamarin Studio\bin\mdtool.exe" setup pack bin\Debug\MonoDevelopSimplePadAddin.dll
